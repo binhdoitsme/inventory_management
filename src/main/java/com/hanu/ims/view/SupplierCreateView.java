c779f8ff01a55b2198e785470d341dc76fee7e7f
binhdoitsme <binhdoitsme@gmail.com>
2020-05-24 08:41:49 +0700

    binhdh: forced supplier's phone to be digit-only

--- src/main/java/com/hanu/ims/view/SupplierCreateView.java
+++ src/main/java/com/hanu/ims/view/SupplierCreateView.java
@@ -4,6 +4,7 @@ import com.hanu.ims.controller.ProductController;
 import com.hanu.ims.controller.SupplierController;
 import com.hanu.ims.model.domain.Category;
 import com.hanu.ims.model.domain.Supplier;
+import com.hanu.ims.util.modal.ModalService;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
@@ -11,20 +12,25 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.function.UnaryOperator;
 
 import static com.hanu.ims.util.modal.ModalService.showAlertDialog;
 import static com.hanu.ims.util.modal.ModalService.showLoadingDialog;
 
 public class SupplierCreateView extends Stage {
     private static final String FXML_FILE_NAME = "supplier_create_view.fxml";
+    private static final UnaryOperator<TextFormatter.Change> TEXT_FILTER = change -> {
+        String text = change.getText();
+        if (text.matches("[0-9]*")) {
+            return change;
+        }
+        return null;
+    };
 
     private static SupplierController controller;
     private static ProductController productController;
@@ -51,12 +57,25 @@ public class SupplierCreateView extends Stage {
         loader.setController(this);
         Parent root = loader.load();
         setScene(new Scene(root));
+        supplierPhoneInput.setTextFormatter(new TextFormatter<>(TEXT_FILTER));
+        supplierPhoneInput.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.length() > 10) {
+                supplierPhoneInput.setText(oldValue);
+            }
+        });
     }
 
     public void onSubmit() {
         String name = supplierNameInput.getText().trim();
         String phone = supplierPhoneInput.getText().trim();
         String address = supplierAddressInput.getText().trim();
+
+        // validation
+        if (name.isEmpty() || phone.isEmpty() || address.isEmpty()) {
+            ModalService.showAlertDialog("All fields must be filled!");
+            return;
+        }
+
         Supplier supplier = new Supplier(name, phone, address, true);
 
         var loadingDialog = showLoadingDialog(getOwner());
